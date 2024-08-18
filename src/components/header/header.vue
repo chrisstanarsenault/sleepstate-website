851efb66f53753cdb75e2d79e0e449c1e8192f6b
Chris Arsenault <chrisstanarsenault@gmail.com>
2024-08-18 00:50:38 -0400

    refactor: :recycle: bit of code cleanup, added assets, added postcss, added basic landing page

---
+++ src/components/header/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="w-full flex justify-between px-20 py-10 relative">
+    <h1 class="text-2xl text-white">
+      Sleepstate
+    </h1>
+
+    <div>
+      <nav class="text-lg space-x-4">
+        <RouterLink to="/">
+          Home
+        </RouterLink>
+
+        <RouterLink to="/music">
+          Music
+        </RouterLink>
+
+        <RouterLink to="/shows">
+          Shows
+        </RouterLink>
+
+        <RouterLink to="/about">
+          About
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
